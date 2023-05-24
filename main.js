17f40de4ad81a722fd6f4f1f39f8ac14fde0f1be
arielcoral <123407136+arielcoral@users.noreply.github.com>
2023-05-24 12:00:52 +0300

    ariel

---
+++ main.js
@@ -0,0 +1,31 @@
+// משימה ראשונה
+
+// const str = `The Naming of Cats is a difficult matter,
+// It isn't just one of your holiday games;
+// You may think at first I'm as mad as a hatter
+// When I tell you, a cat must have THREE DIFFERENT NAMES.
+// First of all, there's the name that the family use daily,
+// Such as Peter, Augustus, Alonzo or James,
+// Such as Victor or Jonathan, George or Bill Bailey—
+// All of them sensible everyday names.
+// There are fancier names if you think they sound sweeter,
+// Some for the gentlemen, some for the dames:
+// Such as Plato, Admetus, Electra, Demeter—
+// But all of them sensible everyday names.
+// But I tell you, a cat needs a name that's particular,
+// A name that's peculiar, and more dignified,
+// Else how can he keep up his tail perpendicular,
+// Or spread out his whiskers, or cherish his pride?`;
+
+// const regex = /[a-z]+.?$/mgi
+// console.log(str.match(regex))
+
+// משימה שניה
+// const text = "ariel180-_";
+// const regex = /^[a-z\d_-]{3,16}$/
+// console.log(text.match(regex))
+
+// משימה שלישית
+// const text = "aeio45"
+// const regex = /^[aeiouAEIOU]+$/
+// console.log(text.match(regex))
